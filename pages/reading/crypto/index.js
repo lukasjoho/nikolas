740581b2394a2ae0e6359ec6ad6b93ca79f39c16
lukasjoho <social@lukashoppe.com>
2021-02-21 03:06:50 +0100

    Convert link to animated link in crypto

--- pages/reading/crypto/index.js
+++ pages/reading/crypto/index.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import { cryptoList } from './cryptoList';
 import Breakpoint from '../../../components/_breakpoints';
 import { motion } from 'framer-motion';
+import { FaLink } from 'react-icons/fa';
 
 export const getStaticProps = async ({ params }) => {
 	return {
@@ -13,14 +14,54 @@ export const getStaticProps = async ({ params }) => {
 		},
 	};
 };
+const StyledListItem = styled.a`
+	color: inherit;
+	text-decoration: none;
+
+	li {
+		margin-bottom: 4rem;
+		display: flex;
+		align-items: center;
+
+		div {
+			p {
+				transition: 0.3s ease;
+			}
+			p:last-child {
+				font-weight: 300;
+				font-size: 1.6rem;
+			}
+		}
+		span {
+			position: absolute;
+			opacity: 0;
+			transition: 0.3s ease;
+			margin-left: -2rem;
+		}
+	}
+	&:hover {
+		span {
+			opacity: 1;
+			margin-left: -2.5rem;
+		}
+		p {
+			margin-left: 0.5rem;
+		}
+	}
+`;
 const ListItem = ({ article }) => {
 	return (
-		<a href={article.link}>
+		<StyledListItem href={article.link} target='_blank'>
 			<motion.li variants={item}>
-				<p>{article.tagline}</p>
-				<p>{article.author}</p>
+				<span>
+					<FaLink />
+				</span>
+				<div>
+					<p>{article.tagline}</p>
+					<p>{article.author}</p>
+				</div>
 			</motion.li>
-		</a>
+		</StyledListItem>
 	);
 };
 const Ul = styled(motion.ul)`
@@ -28,19 +69,6 @@ const Ul = styled(motion.ul)`
 	padding-left: 0rem;
 	@media ${Breakpoint.md} {
 		padding-left: 3rem;
-	}
-	a {
-		color: inherit;
-		text-decoration: none;
-
-		li {
-			margin-bottom: 4rem;
-
-			p:last-child {
-				font-weight: 300;
-				font-size: 1.6rem;
-			}
-		}
 	}
 `;
 
@@ -77,6 +105,7 @@ const CryptoPage = ({ cryptos }) => {
 			textButton="Let's talk crypto"
 		>
 			<Toggle />
+
 			<Ul variants={container} initial='hidden' animate='show'>
 				{list}
 			</Ul>
