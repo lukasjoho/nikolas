45459a166e3d1d4dfe754d310697eed792c77a7e
lukasjoho <social@lukashoppe.com>
2021-02-24 22:40:26 +0100

    Add books animation

--- components/Toggle.js
+++ components/Toggle.js
@@ -1,62 +1,75 @@
-import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import Breakpoint from './_breakpoints';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
+import React, { useState } from 'react';
+const UlContainer = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	@media ${Breakpoint.lg} {
+		justify-content: flex-start;
+	}
+`;
 const Ul = styled.ul`
 	display: inline-flex;
 	list-style-type: none;
-	padding: 0;
 	margin-bottom: 4rem;
 	margin-top: 5rem;
-	margin-left: -2rem;
 	/* margin-left: -3rem; */
+	position: relative;
+	border-radius: 10px;
 
 	@media ${Breakpoint.lg} {
-		margin-left: 0rem;
+		margin-left: 3rem;
 		margin-top: 0rem;
 	}
+	width: 20rem;
+
 	li {
-		padding: 1rem 2rem;
+		padding: 1rem 0rem;
 		position: relative;
 		cursor: pointer;
+		flex: 1 1 0px;
+		width: 50%;
+		text-align: center;
 		@media ${Breakpoint.lg} {
-			padding: 1rem 3rem;
+			padding: 1rem 0rem;
 		}
 	}
 `;
 const Active = styled(motion.div)`
 	position: absolute;
 	z-index: -1;
-	width: 100%;
+	width: 50%;
 	height: 100%;
 	background: red;
 	top: 0;
-	left: 0;
+	left: 0%;
 	background: #27303d;
 	border-radius: 1rem;
+	transition: 0.3s ease;
+	margin-left: ${(props) => (props.toggle === 'books' ? '0%' : '50%')};
 `;
-const SuperLink = ({ path, text }) => {
-	const router = useRouter();
-	console.log(router.route);
+
+const SuperLink = ({ text, toggleState, id }) => {
 	return (
-		<Link href={path}>
-			<li>
-				{text}
-				{router.route === path && <Active layoutId='active' />}
-			</li>
-		</Link>
+		<li id={id} onClick={(e) => toggleState(e)}>
+			{text}
+		</li>
 	);
 };
 
-const Toggle = () => {
+const Toggle = ({ toggle, toggleState }) => {
 	return (
-		<Ul>
-			<SuperLink path='/reading/books' text='Books' />
-			<SuperLink path='/reading/crypto' text='Crypto' />
-		</Ul>
+		<UlContainer>
+			<Ul id='toggler'>
+				<Active toggle={toggle} />
+				<SuperLink id='books' text='Books' toggleState={toggleState} />
+				<SuperLink id='crypto' text='Crypto' toggleState={toggleState} />
+			</Ul>
+		</UlContainer>
 	);
 };
 export default Toggle;
--- pages/reading/books/index.js
+++ pages/reading/books/index.js
@@ -1,22 +1,73 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
-
 import Layout from '../../../components/Layout';
 import Toggle from '../../../components/Toggle';
 import Link from 'next/link';
 import { bookList } from '../../../data/bookList';
+import { cryptoList } from '../../../data/cryptoList';
+
 import Breakpoint from '../../../components/_breakpoints';
 import toast, { Toaster } from 'react-hot-toast';
 import IconBook from '../../../public/icon-book-closed.svg';
+import React, { useState } from 'react';
+import { FaLink } from 'react-icons/fa';
+
 export const getStaticProps = async ({ params }) => {
 	return {
 		props: {
 			books: bookList,
+			cryptos: cryptoList,
 		},
 	};
 };
+const StyledListItem = styled.a`
+	color: inherit;
+	text-decoration: none;
+
+	li {
+		margin-bottom: 4rem;
+		display: flex;
+		align-items: center;
+
+		div {
+			p {
+				transition: 0.3s ease;
+			}
+			p:last-child {
+				font-weight: 300;
+				font-size: 1.6rem;
+			}
+		}
+		span {
+			position: absolute;
+			opacity: 0;
+			transition: 0.3s ease;
+			margin-left: -2rem;
+		}
+	}
+	&:hover {
+		color: #c7f1fd;
+		text-shadow: 0px 0px 2px #c7f1fd;
+		span {
+			opacity: 1;
+			margin-left: -2.5rem;
+		}
+		p {
+			margin-left: 0.5rem;
+		}
+	}
+`;
 const gap = '1.2rem';
-const Ul = styled.ul`
+const UlCrypto = styled(motion.ul)`
+	list-style-type: none;
+	padding-left: 0rem;
+	margin-bottom: -4rem;
+
+	@media ${Breakpoint.lg} {
+		padding-left: 3rem;
+	}
+`;
+const Ul = styled(motion.ul)`
 	display: inline-flex;
 	list-style-type: none;
 	padding-left: 0;
@@ -42,7 +93,6 @@ const Ul = styled.ul`
 		@media ${Breakpoint.md} {
 			margin-bottom: 6rem;
 		}
-
 		cursor: pointer;
 		&:hover {
 			opacity: 1;
@@ -62,7 +112,12 @@ const Ul = styled.ul`
 			display: flex;
 			justify-content: center;
 			align-items: center;
+			transition: 0.3s ease;
+			filter: brightness(50%);
 
+			&:hover {
+				filter: brightness(100%);
+			}
 			img {
 				border-radius: 1rem;
 				box-shadow: 0 2rem 3rem -1rem black;
@@ -71,45 +126,111 @@ const Ul = styled.ul`
 		}
 	}
 `;
+const ListItem = ({ article }) => {
+	return (
+		<StyledListItem href={article.link} target='_blank'>
+			<motion.li variants={item}>
+				<span>
+					<FaLink />
+				</span>
+				<div>
+					<p>{article.tagline}</p>
+					<p>{article.author}</p>
+				</div>
+			</motion.li>
+		</StyledListItem>
+	);
+};
+const BookItem = ({ book }) => {
+	return (
+		<Link href={`books/${book.slug}`}>
+			<motion.li variants={bookitem} onClick={() => toast.dismiss()}>
+				<div>
+					<motion.img layoutId={book.slug} src={book.image} />
+				</div>
+			</motion.li>
+		</Link>
+	);
+};
+
+const container = {
+	hidden: { opacity: 1 },
+	show: {
+		opacity: 1,
+
+		transition: {
+			staggerChildren: 0.05,
+		},
+	},
+};
 const item = {
-	hidden: { opacity: 0, y: -100 },
+	hidden: { opacity: 0, y: -10 },
+	show: { opacity: 1, y: 0 },
+};
+const bookitem = {
+	hidden: { opacity: 0, y: -10 },
 	show: { opacity: 1, y: 0 },
 };
 
-const ReadingPage = ({ books }) => {
+const ReadingPage = ({ books, cryptos }) => {
 	const notify = () => toast('Pop a book of the shelf!');
-
-	const list = books.map((book) => (
-		<Link
-			href={`books/${book.slug}`}
-			onClick={() => console.log('link clicked')}
-		>
-			<li onClick={() => toast.dismiss()}>
-				<div>
-					<motion.img layoutId={book.slug} src={book.image}></motion.img>
-				</div>
-			</li>
-		</Link>
-	));
+	const [toggle, setToggle] = useState('books');
+	const handleState = (e) => {
+		setToggle(e.target.id);
+		console.log(toggle);
+	};
+	// const listBooks = books.map((book) => (
+	// 	<Link href={`books/${book.slug}`}>
+	// 		<li onClick={() => toast.dismiss()}>
+	// 			<div>
+	// 				<motion.img layoutId={book.slug} src={book.image}></motion.img>
+	// 			</div>
+	// 		</li>
+	// 	</Link>
+	// ));
+	const listBooks = books.map((book) => <BookItem book={book} />);
+	const listCrypto = cryptos.map((article) => <ListItem article={article} />);
 
 	return (
 		<Layout
-			title='My Interests'
-			superTitle='Reading'
+			title={toggle === 'books' ? 'I love to read' : 'Disruption'}
+			superTitle={toggle === 'books' ? 'Reading' : 'Crypto'}
 			text={
-				<>
-					To me reading is the greatest source of inspiration and knowledge.
-					Each book and every paper provides me with a new perspective and I
-					seek to evolve from learning about what other people have already
-					figured out. Check out my favorite books and the best research on
-					crypto
-				</>
+				toggle === 'books' ? (
+					<>
+						To me reading is the greatest source of inspiration and knowledge.
+						Each book and every paper provides me with a new perspective and I
+						seek to evolve from learning about what other people have already
+						figured out. Check out my favorite books and the best research on
+						crypto
+					</>
+				) : (
+					<>
+						crypto crypto a crypto crypto a crypto crypto a crypto crypto a
+						crypto crypto a crypto crypto a crypto crypto a crypto crypto a
+						crypto crypto a crypto crypto acrypto crypto a crypto crypto a
+						crypto crypto a crypto crypto a crypto crypto a
+					</>
+				)
 			}
-			textButton='Pick A Book'
+			textButton={toggle === 'books' ? 'Pick A Book' : "Let's Talk Crypto"}
 			toaster={notify}
+			link={
+				toggle === 'crypto'
+					? 'https://www.linkedin.com/in/nikolas-schoneweg-bb12b8166/'
+					: null
+			}
 		>
-			<Toggle />
-			<Ul>{list}</Ul>
+			<Toggle toggle={toggle} toggleState={handleState} />
+			{toggle === 'books' ? (
+				<Ul variants={container} initial='hidden' animate='show'>
+					{listBooks}
+				</Ul>
+			) : (
+				<UlCrypto variants={container} initial='hidden' animate='show'>
+					{listCrypto}
+				</UlCrypto>
+			)}
 			<Toaster
 				position='bottom-center'
 				toastOptions={{
